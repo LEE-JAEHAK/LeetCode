2cc8395c6f62d5f30108e217b89438ca04aa0f42
LEE-JAEHAK <60100786+LEE-JAEHAK@users.noreply.github.com>
2023-04-19 13:19:49 +0900

    Time: 41 ms (86.16%), Space: 15.5 MB (21.80%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        res = ''
+        for i in s:
+            if not (i.isalpha() or i.isnumeric()):
+                continue
+            res += i
+        res = res.lower()
+        a = list(res)
+        return a == list(reversed(a))
